25f18082e39bec9cb362537be7dacbee97c346ab
samen93 <samen3393@gmail.com>
2021-11-03 15:01:41 +0200

    fix: testing without repos

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -33,7 +33,7 @@ module "test" {
   # add all optional arguments that create additional resources
 
   plaintext_value         = "plain-test"
-  selected_repository_ids = [data.github_repository.repo.repo_id]
+  #selected_repository_ids = [data.github_repository.repo.repo_id]
 
   # add most/all other optional arguments
 
